67b66a56cac220b13a49190ebcf3552720a64de9
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-04-18 23:47:21 +0200

    Add the vat field to the product type

--- app/graphql/types/product_type.rb
+++ app/graphql/types/product_type.rb
@@ -6,4 +6,11 @@ Types::ProductType = GraphQL::ObjectType.define do
   field :name, types.String
   field :price,   types.Int
   field :product_image, Types::ProductImageType
+
+  field :vat, types.Float, 'value added tax' do
+    argument :vat_rate, !types.Float, 'Vat percentage'
+    resolve -> (obj, args, ctx) do
+      obj.price * args[:vat_rate]
+    end
+  end 
 end
