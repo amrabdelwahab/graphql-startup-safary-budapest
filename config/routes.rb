b42b56a55b0833adb4508c6df2c6a3d4d31049ed
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2017-07-05 17:17:09 +0200

    Mount graphiql route on all envs

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   resources :product_images
   resources :products
   resources :shops
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
 
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
